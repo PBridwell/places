4a5ebf3547b83eaa2a7723dc90003c305e432361
unknown <pbrid32@gmail.com>
2021-01-05 14:39:54 -0600

    styles user item into card. adds router link

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import NewPlace from './places/pages/NewPlace';
 import Users from './user/pages/Users';
 
 const App = () => {
 	return (
 		<Router>
-			<Route path='/' exact>
-				<Users />
-			</Route>
-			<Redirect to='/' />
+			<Switch>
+				<Route path='/' exact>
+					<Users />
+				</Route>
+				<Route path='/places/new' exact>
+					<NewPlace />
+				</Route>
+				<Redirect to='/' />
+			</Switch>
 		</Router>
 	);
 };
