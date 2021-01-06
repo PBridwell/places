0ddee4f3162056c8177d9e2ff53344641831f8df
unknown <pbrid32@gmail.com>
2021-01-06 13:46:41 -0600

    adds dummy data to places. renders on my places page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
 import Users from './user/pages/Users';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
@@ -13,6 +14,9 @@ const App = () => {
 				<Switch>
 					<Route path='/' exact>
 						<Users />
+					</Route>
+					<Route path='/:userId/places' exact>
+						<UserPlaces />
 					</Route>
 					<Route path='/places/new' exact>
 						<NewPlace />
--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,6 +1,38 @@
 import React from 'react';
 
+import PlaceList from '../components/PlaceList';
+
+// dummy places list
+const DUMMY_PLACES = [
+	{
+		id: 'p1',
+		title: 'Grand Canyon',
+		description: 'A really very large hole',
+		imageUrl:
+			'https://www.nps.gov/grca/planyourvisit/images/0531fyp.jpg?maxwidth=650&autorotate=false',
+		address: 'S Entrance Rd, Grand Canyon Village, AZ 86023',
+		location: {
+			lat: 36.0591278,
+			lng: -112.1093538,
+		},
+		creator: 'u1',
+	},
+	{
+		id: 'p2',
+		title: 'Grand Canyon',
+		description: 'A really very large hole',
+		imageUrl:
+			'https://www.nps.gov/grca/planyourvisit/images/0531fyp.jpg?maxwidth=650&autorotate=false',
+		address: 'S Entrance Rd, Grand Canyon Village, AZ 86023',
+		location: {
+			lat: 36.0591278,
+			lng: -112.1093538,
+		},
+		creator: 'u2',
+	},
+];
+
 const UserPlaces = () => {
-	return;
+	return <PlaceList items={DUMMY_PLACES} />;
 };
 export default UserPlaces;
