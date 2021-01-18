bf0ff781934c33600ca067d370d7f93077736684
unknown <pbrid32@gmail.com>
2021-01-18 11:58:57 -0600

    sets inital inputs for update place

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-import './UpdatePlace.css';
+import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
+import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../shared/util/validators';
+
+// import './UpdatePlace.css';
 
 const DUMMY_PLACES = [
 	{
@@ -45,7 +49,34 @@ const UpdatePlace = () => {
 		);
 	}
 
-	return <h2>Update this face</h2>;
+	return (
+		<form>
+			<Input
+				id='title'
+				element='input'
+				type='text'
+				label='Title'
+				validators={[VALIDATOR_REQUIRE()]}
+				errorText='Please Enter a Valid Title.'
+				onInput={() => {}}
+				value={identifiedPlace.title}
+				valid={true}
+			/>
+			<Input
+				id='description'
+				element='textarea'
+				label='Description'
+				validators={[VALIDATOR_MINLENGTH(5)]}
+				errorText='Please Enter at least 5 characters.'
+				onInput={() => {}}
+				value={identifiedPlace.description}
+				valid={true}
+			/>
+			<Button type='submit' disabled={true}>
+				Update
+			</Button>
+		</form>
+	);
 };
 
 export default UpdatePlace;
